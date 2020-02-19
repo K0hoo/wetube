66b69f2b15ed3c4808191e2c7713ae2a5cf22d77
K0hoo <caitlyn201350@gmail.com>
2020-02-19 19:46:10 +0900

    Controller

---
+++ controller/videoConteoller.js
@@ -0,0 +1,8 @@
+export const home = (req, res) => res.send("Home");
+export const search = (req, res) => res.send("Search");
+
+export const videoIndex = (req, res) => res.send("Videos");
+export const videoUpload = (req, res) => res.send("Upload Video");
+export const videoDetail = (req, res) => res.send("Video Detail");
+export const videoEdit = (req, res) => res.send("Edit Video");
+export const videoDelete = (req, res) => res.send("Delete Video");
--- router/videoRouter.js
+++ router/videoRouter.js
@@ -1,12 +1,13 @@
 import express from "express";
 import routes from "../routes";
+import { videoIndex, videoUpload, videoDetail, videoEdit, videoDelete } from "../controller/videoConteoller";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, (req, res) => res.send("videos"));
-videoRouter.get(routes.upload, (req, res) => res.send("upload video"));
-videoRouter.get(routes.videoDetail, (req, res) => res.send("video detail"));
-videoRouter.get(routes.editVideo, (req, res) => res.send("edit video"));
-videoRouter.get(routes.deleteVideo, (req, res) => res.send("delete video"));
+videoRouter.get(routes.videos, videoIndex);
+videoRouter.get(routes.upload, videoUpload);
+videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.editVideo, videoEdit);
+videoRouter.get(routes.deleteVideo, videoDelete);
 
 export default videoRouter;
