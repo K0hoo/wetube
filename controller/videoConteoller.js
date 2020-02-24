4ae3e8348cea5038134ba57137600bb50b1c4f2a
K0hoo <caitlyn201350@gmail.com>
2020-02-25 04:07:45 +0900

    pug settings

--- controller/videoConteoller.js
+++ controller/videoConteoller.js
@@ -1,8 +1,8 @@
-export const home = (req, res) => res.send("Home");
-export const search = (req, res) => res.send("Search");
+export const home = (req, res) => res.render("home");
+export const search = (req, res) => res.render("search");
 
-export const videoIndex = (req, res) => res.send("Videos");
-export const videoUpload = (req, res) => res.send("Upload Video");
-export const videoDetail = (req, res) => res.send("Video Detail");
-export const videoEdit = (req, res) => res.send("Edit Video");
-export const videoDelete = (req, res) => res.send("Delete Video");
+export const videoIndex = (req, res) => res.render("videos");
+export const videoUpload = (req, res) => res.render("uploadVideo");
+export const videoDetail = (req, res) => res.render("videoDetail");
+export const videoEdit = (req, res) => res.render("editVideo");
+export const videoDelete = (req, res) => res.render("deleteVideo");
