5330b4cbdb08dd15a114351c27e5be23857c584a
K0hoo <caitlyn201350@gmail.com>
2020-02-28 01:34:12 +0900

    Search

--- controller/videoConteoller.js
+++ controller/videoConteoller.js
@@ -1,8 +1,16 @@
 export const home = (req, res) => res.render("home", { pageTitle: "Home" });
-export const search = (req, res) => res.render("search", { pageTitle: "Search" });
+
+export const search = (req, res) => {
+    const { query: { term: searchingBy } } = req;
+    res.render("search", { pageTitle: "Search", searchingBy });
+}
 
 export const videoIndex = (req, res) => res.render("videos", { pageTitle: "Videos" });
+
 export const videoUpload = (req, res) => res.render("uploadVideo", { pageTitle: "Upload Video" });
+
 export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
+
 export const videoEdit = (req, res) => res.render("editVideo", { pageTitle: "Edit Video" });
+
 export const videoDelete = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video" });
